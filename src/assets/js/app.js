19d1796a883fb3121459f6d6ce064f78bf3d8ca5
paweltar <paweltar@gmail.com>
2017-02-02 18:41:04 +0100

    Minor fix in script that changes navbar color on scroll

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -2,7 +2,7 @@ $(document).foundation();
 
 $(function(){
     $(document).scroll(function(){
-        if($(this).scrollTop() >= $('.offer-cards__section').offset().top - 50) {
+        if($(this).scrollTop() >= $('.offer-cards__section').offset().top - $(".navigation").height()) {
             $(".navigation").addClass('color--alt');
         } else {
             $(".navigation").removeClass('color--alt');
