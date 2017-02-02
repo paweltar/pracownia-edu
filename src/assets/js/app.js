2bb93578c35ba1efaaa2530760f723d2219bf7f6
paweltar <paweltar@gmail.com>
2017-02-02 18:36:50 +0100

    Topbar changes color on scroll properly

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,1 +1,11 @@
 $(document).foundation();
+
+$(function(){
+    $(document).scroll(function(){
+        if($(this).scrollTop() >= $('.offer-cards__section').offset().top - 50) {
+            $(".navigation").addClass('color--alt');
+        } else {
+            $(".navigation").removeClass('color--alt');
+        }
+    });
+});
